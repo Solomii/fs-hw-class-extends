7757b683b03f395fa7be122abd962732160130ac
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-26 19:20:24 +0200

    Add checks and chenged buy method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  Створити клас Товар з властивостями: назва, ціна, валюта, кількість,
 і методами: повернути інформацію про товар і купити товар(метод приймає кількість одиниць товару і повертає суму).
@@ -21,6 +22,10 @@ class Product {
             throw new TypeError("name must be string!");
         }
 
+        if (value.length === 0) {
+            throw new RangeError("missing name");
+        }
+
         this._name = value;
     }
     get name() {
@@ -32,6 +37,10 @@ class Product {
             throw new TypeError("price must be number!");
         }
 
+        if (value < 0) {
+            throw new RangeError("price can't be negative");
+        }
+
         this._price = value;
     }
 
@@ -41,7 +50,11 @@ class Product {
 
     set currency(value) {
         if (typeof value !== "string") {
-            throw new TypeError("name must be string!");
+            throw new TypeError("currency must be string!");
+        }
+
+        if (value.length === 0) {
+            throw new RangeError("missing currency");
         }
 
         this._currency = value;
@@ -55,6 +68,10 @@ class Product {
             throw new TypeError("quantity must be number!");
         }
 
+        if (value < 0) {
+            throw new RangeError("quantity can't be negative");
+        }
+
         this._quantity = value;
     }
 
@@ -66,22 +83,27 @@ class Product {
         return `Name product: ${this.name} - ${this.price}${this.currency}, quantity in stock: ${this.quantity}`;
     }
 
-    buy() {
-        //доробити!!!
-        return `you need pay: ${this._price * this._quantity}${this._currency}`;
+    buy(amountValue) {
+        if (
+            amountValue === "" ||
+            amountValue === null ||
+            amountValue <= 0 ||
+            Number.isNaN(amountValue)
+        ) {
+            return "error";
+        }
+        if (this.quantity <= amountValue) {
+            return null;
+        }
+        this.quantity -= amountValue;
+        return `you need to pay: ${this.price * amountValue}${
+            this.currency
+        } for ${amountValue} ${this.name}`;
     }
 
     static isProduct(obj) {
         return obj instanceof Product;
     }
-}
-
-try {
-    const productOne = new Product("eggs", 8, "uah", 10);
-    console.log(productOne.getInformationAboutProduct());
-    console.log(productOne.buy(20, 10));
-} catch (error) {
-    console.log(error);
 }
 
 class PhysicalProduct extends Product {
@@ -93,6 +115,10 @@ class PhysicalProduct extends Product {
     set weight(value) {
         if (typeof value !== "number") {
             throw new TypeError("weight must be number!");
+        }
+
+        if (value < 0) {
+            throw new RangeError("weight can't be negative");
         }
 
         this._weight = value;
@@ -107,14 +133,6 @@ class PhysicalProduct extends Product {
     }
 }
 
-try {
-    const productTwo = new PhysicalProduct("onion", 20, "uah", 20, 5);
-    console.log(productTwo.getInformationAboutProduct());
-    console.log(productTwo.buy(20, 3));
-} catch (error) {
-    console.log(error);
-}
-
 class VirtualProduct extends Product {
     constructor(name, price, currency, quantity, memory) {
         super(name, price, currency, quantity);
@@ -124,6 +142,10 @@ class VirtualProduct extends Product {
     set memory(value) {
         if (typeof value !== "number") {
             throw new TypeError("weight must be number!");
+        }
+
+        if (value < 0) {
+            throw new RangeError("memory can't be negative");
         }
 
         this._memory = value;
@@ -139,9 +161,97 @@ class VirtualProduct extends Product {
 }
 
 try {
-    const productThree = new VirtualProduct("virtualGood", 100, "uah", 5, 20);
+    const productOne = new Product("eggs", 8, "uah", 10);
+    console.log(productOne.getInformationAboutProduct());
+    console.log(productOne.buy(8));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    const productOnePriceNegative = new Product("eggs", -8, "uah", 10);
+    console.log(productOnePriceNegative.getInformationAboutProduct());
+    console.log(productOnePriceNegative.buy(8));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+  const productOnePriceTypeofError = new Product("eggs", "dfd", "uah", 10);
+  console.log(productOnePriceTypeofError.getInformationAboutProduct());
+  console.log(productOnePriceTypeofError.buy(8));
+} catch (error) {
+  console.log(error);
+}
+
+try {
+    const productOneQuantityNegative = new Product("eggs", 8, "uah", -10);
+    console.log(productOneQuantityNegative.getInformationAboutProduct());
+    console.log(productOneQuantityNegative.buy(8));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    const productOneMissingName = new Product("", 8, "uah", 10);
+    console.log(productOneMissingName.getInformationAboutProduct());
+    console.log(productOneMissingName.buy(8));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+  const productOneTypeofErrorName = new Product(3, 8, "uah", 10);
+  console.log(productOneTypeofErrorName.getInformationAboutProduct());
+  console.log(productOneTypeofErrorName.buy(8));
+} catch (error) {
+  console.log(error);
+}
+
+try {
+    const productOneMissingCurrency = new Product("eggs", 8, "", 10);
+    console.log(productOneMissingCurrency.getInformationAboutProduct());
+    console.log(productOneMissingCurrency.buy(8));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+  const productOneTypeofErrorCurrency = new Product("eggs", 8, null, 10);
+  console.log(productOneTypeofErrorCurrency.getInformationAboutProduct());
+  console.log(productOneTypeofErrorCurrency.buy(8));
+} catch (error) {
+  console.log(error);
+}
+
+try {
+    const productTwo = new PhysicalProduct("onion", 20, "uah", 20, 5);
+    console.log(productTwo.getInformationAboutProduct());
+    console.log(productTwo.buy(3));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    const productTwoNegative = new PhysicalProduct("onion", 20, "uah", 20, -5);
+    console.log(productTwoNegative.getInformationAboutProduct());
+    console.log(productTwoNegative.buy(3));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    const productThree = new VirtualProduct("eBook", 20, "usd", 5, 20);
     console.log(productThree.getInformationAboutProduct());
-    console.log(productThree.buy(20, 3));
+    console.log(productThree.buy(2));
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    const productThreeNegative = new VirtualProduct("eBook", 20, "usd", 5, -20);
+    console.log(productThreeNegative.getInformationAboutProduct());
+    console.log(productThreeNegative.buy(2));
 } catch (error) {
     console.log(error);
 }
